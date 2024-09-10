62ef000b1f8b818795d2f8b7d321b1da70d8e9b4
Joon Lee <l33j00n325@gmail.com>
2024-09-10 14:55:34 -0700

    add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
 import "@mantine/core/styles.css";
 import { theme } from "./theme";
+import { createClient, Session } from "@supabase/supabase-js";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supabase/auth-ui-react";
+
+const supabase = createClient(import.meta.env.VITE_APP_SUPABASE_URL, import.meta.env.VITE_APP_SUPABASE_KEY)
 
 export default function App() {
-  return <MantineProvider theme={theme} forceColorScheme="dark">App</MantineProvider>;
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    })
+
+    const {
+      data: { subscription }
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    })
+
+    return () => {
+      subscription.unsubscribe();
+    }
+  }, [])
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+  } else {
+    return <MantineProvider theme={theme} forceColorScheme="dark">App</MantineProvider>;
+  }
 }
