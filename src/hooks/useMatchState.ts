7e2e43fcba1faadb8ba33f1629e47a8d8ee82410
Joon Lee <l33j00n325@gmail.com>
2024-11-08 18:13:02 -0800

    fix no cap options at end of match

--- src/hooks/useMatchState.ts
+++ src/hooks/useMatchState.ts
@@ -1,12 +1,20 @@
 export default (minute: number) => {
   const { sortedCaps } = useCapHelpers();
 
+  const match = useAtomValue(matchAtom)!;
+  const endOfMatch = useMemo(
+    () => (match.extra_time ? 120 : 90),
+    [match.extra_time],
+  );
+
   const capsAtMinute = useMemo(
     () =>
       sortedCaps.filter(
-        (cap) => cap.start_minute <= minute && minute < cap.stop_minute,
+        (cap) =>
+          cap.start_minute <= minute &&
+          (minute < cap.stop_minute || cap.stop_minute === endOfMatch),
       ),
-    [minute, sortedCaps],
+    [minute, sortedCaps, endOfMatch],
   );
 
   const activeCaps = useMemo(
