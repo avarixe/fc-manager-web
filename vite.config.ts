ada6b8eb7871fd7056bb33d637668adb588701fe
Joon Lee <joon@innocaption.com>
2025-11-13 10:30:08 -0800

    Revert "add react compiler"
    This reverts commit 9c4ba47dc8eeb8e2ff1b93174543a7c8b4676e2b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
@@ -9,17 +9,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter({ target: "react", autoCodeSplitting: true }),
     react({
       babel: {
-        plugins: [
-          jotaiDebugLabel,
-          jotaiReactRefresh,
-          "babel-plugin-react-compiler",
-        ],
+        plugins: [jotaiDebugLabel, jotaiReactRefresh],
       },
     }),
     UnoCSS(),
+    TanStackRouterVite(),
   ],
   resolve: {
     alias: {
